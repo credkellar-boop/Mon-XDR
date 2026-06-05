c5cebc5a48c160f15714d5fce4db1185a52d80e9
Darion Kellar <credkellar@gmail.com>
2026-06-05 02:06:14 -0700

    Update listener.go

--- cmd/agent/listener.go
+++ cmd/agent/listener.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-    "github.com/gorilla/websocket"
-    "log"
+	"log"
+
+	"github.com/credkellar-boop/Mon-XDR/pkg/action"
+	"github.com/gorilla/websocket"
 )
 
 func ListenForCommands(serverURL string) {
-    conn, _, err := websocket.DefaultDialer.Dial(serverURL, nil)
-    if err != nil {
-        log.Fatal("Connection to backend failed:", err)
-    }
+	conn, _, err := websocket.DefaultDialer.Dial(serverURL, nil)
+	if err != nil {
+		log.Fatalf("Connection to backend failed: %v", err)
+	}
 
-    for {
-        var cmd struct {
-            Action string `json:"action"` // e.g., "QUARANTINE"
-            Path   string `json:"path"`
-        }
-        
-        // Blocking read: wait for command from the server
-        err := conn.ReadJSON(&cmd)
-        if err != nil {
-            log.Println("Connection dropped:", err)
-            break
-        }
+	for {
+		var cmd struct {
+			Action string `json:"action"` // e.g., "QUARANTINE", "KILL_PROCESS"
+			Path   string `json:"path"`
+		}
 
-        // Execute the command locally
-        switch cmd.Action {
-        case "QUARANTINE":
-            quarantineFile(cmd.Path)
-        case "KILL_PROCESS":
-            killProcess(cmd.Path)
-        }
-    }
+		// Blocking read: wait for command from the server
+		err := conn.ReadJSON(&cmd)
+		if err != nil {
+			log.Println("Connection dropped:", err)
+			break
+		}
+
+		// Execute the command locally
+		switch cmd.Action {
+		case "QUARANTINE":
+			action.QuarantineFile(cmd.Path)
+		case "KILL_PROCESS":
+			action.KillProcess(cmd.Path)
+		}
+	}
 }
